fce0e9a21bf12cf73a1522f8e6e1230f9c596454
crsvalle <cristian.valle@pursuit.org>
2024-04-02 01:10:05 -0400

    refactored validation

--- validations/index.js
+++ validations/index.js
@@ -7,12 +7,13 @@ const participant_fields = [
 const raffle_fields = [
     'name', 'secret_token'
 ]
+
 const validateRaffleNotOver = async (req, res, next) => {
     const { id } = req.params;
-    if (Object.keys(req.body).length > 0){
-        return res.status(400).json({error: `The request body must be empty.`})
+    if (Object.keys(req.body).length > 0) {
+        return res.status(400).json({ error: `The request body must be empty.` })
     }
-    const raffle = await getRaffleById(id); 
+    const raffle = await getRaffleById(id);
     if (raffle.ended) {
         return res.status(400).json({ error: "The raffle has already ended." });
     }
@@ -32,65 +33,48 @@ const validateId = (req, res, next) => {
 const validateRaffleExist = async (req, res, next) => {
     const { id } = res;
     const raffle = await getRaffleById(id);
-    if (!raffle){
-        return res.status(404).json({error: `Cannot find ID: ${id}`})
+    if (!raffle) {
+        return res.status(404).json({ error: `Cannot find raffle with ID: ${id}` })
     }
 
     next();
 }
+const validateFields = (req, res, next, fields) => {
+    const body = req.body;
 
-const validateRaffle = (req, res, next) => {
-    const raffleBody = req.body;
-
-    for (const field of raffle_fields) {
-        if (!raffleBody.hasOwnProperty(field)) {
+    for (const field of fields) {
+        if (!body.hasOwnProperty(field)) {
             return res.status(400).json({
-                error: `${field} is missing or wrong, recevied ${raffleBody[field]}`
-            })
-        }
-    }
-    for (const field in raffleBody) {
-        if (!raffle_fields.includes(field)) {
-            return res.status(400).json({ error: `${field} is not allowed.` })
-        }
-    }
-
-    for (const field in raffleBody) {
-        if (typeof raffleBody[field] !== 'string') {
-            return res.status(400).json({ error: `'${raffleBody[field]}' field must be a string.` })
-        }
-
-    }
-
-    next();
-}
-
-const validateParticipant = (req, res, next) => {
-    const participantBody = req.body;
-
-    for (const field of participant_fields) {
-        if (!participantBody.hasOwnProperty(field)) {
-            return res.status(400).json({
-                error: `${field} is missing or wrong, received ${participantBody[field]}`
+                error: `${field} is missing or wrong, received ${body[field]}`
             });
         }
     }
 
-    for (const field in participantBody) {
-        if (!participant_fields.includes(field)) {
+    for (const field in body) {
+        if (!fields.includes(field)) {
             return res.status(400).json({ error: `${field} is not allowed.` });
         }
     }
 
-    for (const field in participantBody) {
-        if (typeof participantBody[field] !== 'string' && field !== 'phone') {
+    for (const field in body) {
+        if (typeof body[field] !== 'string' && field !== 'phone') {
             return res.status(400).json({ error: `'${field}' field must be a string.` });
         }
     }
-    if (!participantBody['phone']){
-        participantBody['phone'] = null;
+
+    if (!body['phone'] && fields === participant_fields) {
+        body['phone'] = null;
     }
 
     next();
-}
-module.exports = { validateId, validateRaffle, validateRaffleExist, validateRaffleNotOver, validateParticipant}
+};
+
+const validateRaffle = (req, res, next) => {
+    validateFields(req, res, next, raffle_fields);
+};
+
+const validateParticipant = (req, res, next) => {
+    validateFields(req, res, next, participant_fields);
+};
+
+module.exports = { validateId, validateRaffle, validateRaffleExist, validateRaffleNotOver, validateParticipant }
